d35e368e417f2feed8e1df92bb787e9c3011c962
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-02-10 23:52:47 -0300

    Teste de conexão com git heroku

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24763.114160698788!2d-43.83353741155226!3d-17.117819082656517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xab6230efb84b03%3A0xf8120c7ff129066c!2sBocai%C3%BAva%2C%20MG%2C%2039390-000!5e1!3m2!1spt-BR!2sbr!4v1572448714991!5m2!1spt-BR!2sbr" width="1325" height="450" frameborder="0" margin-left="3%;"  allowfullscreen=""></iframe><br>
 
 <div class="btn-group btn-group-toggle  col-sm-12 col-md-12 col-lg-12 col-xg-12" data-toggle="buttons">
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".historia">História</button>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".historia">Teste</button>
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".geografia">Dados Demográficos</button>
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".turismo">Pontos Turísticos</button>
 </div>
