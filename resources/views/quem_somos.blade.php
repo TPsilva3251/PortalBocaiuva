e045adb80a0d2736783e1f922621b0e3cf9a2940
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-02-12 23:56:14 -0300

    criação da pagina quemSomos

---
+++ resources/views/quem_somos.blade.php
@@ -0,0 +1,13 @@
+@include('/layout/template')
+
+<div class="division-s-primary">
+    <h2>O que é o Portal Bocaiuva</h2>
+</div>
+<br><br><br>
+    <p style="margin-left: 9%">Criado em 2021, o Portal Bocaiuva surgiu com o objetivo de apoiar o comécio local aproximando empreendedores e consumidores através do meio digital.</p>
+    <p style="margin: 0% 9% 1% 9%">A principal missão é estar sempre estruturando nossa comunidade online, unida para valorizar nossa cidade,
+                                promovendo noções de cidadania por meio de um modelo de mídia local diferenciado dos convencionais,
+                                 buscando sempre trabalhar com a interação e a participação popular.</p>
+    <p style="margin: 1% 9% 0% 9%">O Portal Bocaiuva tem como proposta ser um serviço de informação e comunicação feito para a comunidade,
+                                    com o intuito de divulgar o comécio local e aproximar os comerciantes dos seu consumidores.</p>
+@include('/layout/footer')
