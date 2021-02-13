f397567163381b9be60d4a821f2d62050814f076
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-02-12 23:55:29 -0300

    criação da pagina historia

---
+++ resources/views/historia.blade.php
@@ -0,0 +1,16 @@
+@include('/layout/template')
+
+<div class="text1 col-sm-12 col-md-12 col-lg-12 col-xg-12" id="historia">
+    <p>Bocaiuva surgiu em 15 de junho de 1553, originada pelas expedições de Spinoza e Navarro. Eles vinham de Porto Seguro à procura de riquezas. Alguns integrantes dessa expedição resolveram permanecer no local por achar as terras promissoras.</p>
+    <p>A bandeira de Fernão Dias Leme iniciou a segunda entrada de pessoas na região. Pertenciam a essa bandeira Matias Cardoso de Almeida e Antônio Gonçalves Figueira.</p>
+    <p>Em 1700 surgiu em Bocaiuva a imagem do Senhor do Bonfim, que também contribuiu para o povoamento da Região.</p>
+    <p>O primeiro nome oficial desta terra foi Curato de Macaúbas, depois Arraial do Senhor do Bonfim, depois, Freguesia do Senhor do Bonfim, Vila do Jequitaí, Vila Nova do Jequitaí.</p>
+    <p>Existem duas versões a respeito da origem do nome "Bocaiuva": uma remete à palmeira Macaúba (nome científico Acrocomia aculeata, também conhecida como bocaiúva) e a outra ao abolicionista e jornalista Quintino Bocaiuva. Entretanto, esse jornalista em nada contribuiu para o nome da cidade, visto que a abundância da palmeira "bocaiúva" na região é o que de fato deu origem ao nome do município mineiro.</p>
+    <p>Em 14 de julho de 1888, o município passa a denominar-se Bocaiúva, com o Coronel Manoel Freire Figueiredo Fonseca.</p>
+    <p>Em 20 de maio de 1947 ocorreu um eclipse do Sol que teve sua faixa cruzando o território brasileiro. A cidade de Bocaiuva (MG) recebeu diversas expedições científicas para a realização de observações, coleta de dados e posteriores estudos sobre o fenômeno. As circunstâncias do imediato pós-Segunda Guerra Mundial conferiram um grande destaque na imprensa nacional e internacional a este evento,
+        produzindo registros em diferentes mídias, Bocaiuva – cidade que recebeu a maioria das expedições científicas para a observação do fenômeno – e para a auto-afirmação de sua identidade como um local de produção de conhecimento.</p>
+    <p>Bocaiuva recebeu uma expedição de importantes pesquisadores e cientistas para o estudo do fenômeno, como o engenheiro e físico estadunidense Lyman James Briggs, diretor do National Bureau of Standards e que liderou a equipe do National Geographic Society, e o astrônomo belgo-americano George Van Biesbroeck, que confirmou a Teoria da Relatividade, em 1952.</p>
+    <p>Especula-se que o físico Albert Einstein iria a Bocaiúva analisar o fenômeno ocorrido, mas fontes afirmam que o cientista desistiu da viagem em último momento.</p>
+</div>
+<br><br><br>
+@include('/layout/footer')
