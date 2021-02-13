840c8af5d1193c077e7496d6ec05ed8c6fd9c8ca
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-02-12 23:58:36 -0300

    alteração no ano 2021

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="division-footer">
     <footer class="page-footer font-small blue col-sm-12 col-md-12 col-lg-12 col-xg-12">
-        <div class="footer navbar-fixed-bottom" style="text-align:right; margin-right:2%;">© 2019  WebSyxs</div>
+        <div class="footer navbar-fixed-bottom" style="text-align:right; margin-right:2%;">© 2021  WebSyxs</div>
     </footer>
 </div>
