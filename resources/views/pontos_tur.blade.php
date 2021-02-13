93b197900e2545babd38f45a314f46a56f6b6a94
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-02-12 23:55:53 -0300

    criação da pagina pontosTuristicos

---
+++ resources/views/pontos_tur.blade.php
@@ -0,0 +1,18 @@
+@include('/layout/template')
+<br>
+<div class="text1" id="turismo">
+    <div class="division-primary" style="text-align:center;">
+        <h3>Turismo</h3>
+    </div>
+    <div class="texto-primary">
+        <p>Teste</p>
+    </div>
+</div>
+<div class="text1 col-sm-12 col-md-12 col-lg-12 col-xg-12" id="turismo">
+    <div class="division-primary" style="text-align:center;">
+        <h3>Trabalho e Rendimento</h3>
+    </div>
+
+</div>
+<br>
+@include('/layout/footer')
