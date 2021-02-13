71ae8c4a1aa1325e9aa57c351e174a8dd1b03ae5
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-02-12 23:54:54 -0300

    criação da página dadosDemograficos

---
+++ resources/views/dados_demo.blade.php
@@ -0,0 +1,25 @@
+@include('/layout/template')
+<br>
+<div class="text1 col-sm-12 col-md-12 col-lg-12 col-xg-12" id="geografia">
+    <div class="division-primary" style="text-align:center;">
+        <h3>População</h3>
+    </div>
+    <div class="texto-primary">
+        <p>População estimada -2019 <strong> 49.979 </strong> pessoas</p>
+        <p>População no útimo censo -2010 <strong> 46.654 </strong> pessoas</p>
+        <p>Densidade demográfica -2010 <strong> 14,45 </strong> hab/km²</p>
+    </div>
+</div>
+<div class="text1 col-sm-12 col-md-12 col-lg-12 col-xg-12" id="geografia">
+    <div class="division-primary" style="text-align:center;">
+        <h3>Trabalho e Rendimento</h3>
+    </div>
+    <div class="texto-primary">
+        <p>Salário médio mensal dos trabalhadores formais -2017 <strong> 1,7 </strong> salários mínimos</p>
+        <p>Pessoal ocupado -2017 <strong> 8.444 </strong> pessoas</p>
+        <p>População ocupada -2017 <strong> 16,8 </strong> %</p>
+        <p>Percentual da população com rendimento nominal mensal per capita de até 1/2 salário mínimo -2010 <strong> 41,4 </strong> %</p>
+    </div>
+</div>
+<br>
+@include('/layout/footer')
