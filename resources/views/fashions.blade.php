2fcb03baee67d6c3f9e549f5bd6f9e9f317cbbb7
thiago <thiagogua2009@gmail.com>
2021-02-14 15:47:59 -0300

    criado input para imagem

--- resources/views/fashions.blade.php
+++ resources/views/fashions.blade.php
@@ -1,7 +1,7 @@
 @include('/layout/template')
 <h1>Modas</h1>
 {{-- <img src="https://thumbnails-photos.amazon.com/v1/thumbnail/olcEAebOTx2n7D5p2k-vGg?viewBox=450%2C800&ownerId=AKPXXKKGQUSCQ" class="img-fluid" alt="Responsive image"> --}}
-<div class="imagens col-sm-12 col-md-12 col-lg-12 col-xg-12">
+{{-- <div class="imagens col-sm-12 col-md-12 col-lg-12 col-xg-12">
     <img src="https://thumbnails-photos.amazon.com/v1/thumbnail/olcEAebOTx2n7D5p2k-vGg?viewBox=450%2C800&ownerId=AKPXXKKGQUSCQ"  width="436" height="300">
     <img src="https://thumbnails-photos.amazon.com/v1/thumbnail/olcEAebOTx2n7D5p2k-vGg?viewBox=450%2C800&ownerId=AKPXXKKGQUSCQ"  width="436" height="300">
     <img src="https://thumbnails-photos.amazon.com/v1/thumbnail/olcEAebOTx2n7D5p2k-vGg?viewBox=450%2C800&ownerId=AKPXXKKGQUSCQ"  width="436" height="300">
@@ -11,4 +11,12 @@
     <img src="https://thumbnails-photos.amazon.com/v1/thumbnail/olcEAebOTx2n7D5p2k-vGg?viewBox=450%2C800&ownerId=AKPXXKKGQUSCQ"  width="436" height="300">
     <img src="https://thumbnails-photos.amazon.com/v1/thumbnail/olcEAebOTx2n7D5p2k-vGg?viewBox=450%2C800&ownerId=AKPXXKKGQUSCQ"  width="436" height="300">
     <img src="https://thumbnails-photos.amazon.com/v1/thumbnail/olcEAebOTx2n7D5p2k-vGg?viewBox=450%2C800&ownerId=AKPXXKKGQUSCQ"  width="436" height="300">
-</div>
+</div> --}}
+<form action="{{ route('imagen') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    <input type="file" name="image">
+    
+    <button type="submit" class="btn btn-success" id="submit">Salvar</button>
+
+</form>
+
