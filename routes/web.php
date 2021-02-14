66849be16165506637e69802735202b8222b7b14
thiago <thiagogua2009@gmail.com>
2021-02-14 15:47:26 -0300

    criada rota para upload de imagens

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('/fashions' , 'FashionsController@index')->name('fashions.index');
+Route::post('/images/store','ImagesController@store')->name('imagen');
 
 Route::get('/historia', function () {
     return view('historia');
@@ -32,3 +33,4 @@ Route::get('/pontos_turisticos', function () {
 Route::get('/quem_somos', function () {
     return view('quem_somos');
 });
+
