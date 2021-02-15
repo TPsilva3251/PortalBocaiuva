e6573f3ec46b0882ea64b709c771315ea9344dbb
Thiago <thiago.silva@sagatechbrasil.com.br>
2021-02-15 00:08:54 -0300

    adição de input imagem

--- resources/views/parceiro_create_edit.blade.php
+++ resources/views/parceiro_create_edit.blade.php
@@ -2,7 +2,15 @@
 <form action="{{ route('create_parceiro') }}" method="post" enctype="multipart/form-data">
     @csrf
     <input type="text" name="nome" placeholder="nome">
-    <input type="text" name="categories_id" placeholder="categories_id">
+    {{-- <input type="text" name="categories_id" placeholder="categories_id"> --}}
+    <select class="form-control input-required"
+            id="categoria" name="categories_id" style="width: 25%" >
+        {{-- <option value=""></option> --}}
+        <option value="" disabled selected>Selecione uma categoria</option>
+        @foreach($categorias as $categoria)
+            <option value="{{ $categoria->id }}">{{ $categoria->nome }}</option>
+        @endforeach
+    </select>
     <input type="text" name="descricao" placeholder="descricao">
     <input type="text" name="localizacao" placeholder="localizacao">
     <input type="text" name="face" placeholder="face">
@@ -10,7 +18,12 @@
     <input type="text" name="whats" placeholder="whats">
     <input type="text" name="email" placeholder="email">
     <br>
-    <input type="file" name="img1">
+    <div class="input-group mb-3 col-sm-3 col-md-3 col-lg-3 col-xg-3">
+        <div class="custom-file">
+          <input type="file" class="custom-file-input "  name="img1" >
+          <label class="custom-file-label" >Choose file</label>
+        </div>
+      </div>
     <br>
     <input type="file" name="img2">
     <br>
